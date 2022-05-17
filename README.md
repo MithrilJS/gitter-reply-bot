1866041d0013c1aed533e265bcfc5b70d918a831
Pierre-Yves <py@py.gy>
2022-05-17 17:56:00 +0200

    Here's the bot
    e Please enter the commit message for your changes. Lines starting

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gitter-reply-bot
 The reply bot for the legacy Mithril chat rooms
+
+The bot is currently active, but refrains from posting.
+
+Uncommenting the `# postMessage $ROOM_ID` in `reply.sh` will enable replies.
---
+++ reply.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
+MESSAGE="\
+Hello, fellow traveler, before you lose yourself in this derelict place, \
+I'll enjoin you to swiftly go to https://mithril.zulip.com where life abounds \
+and conversations flow.\n\nAlso, please spread the word around so that others don't get astray... \
+update the old links that lead you here, or signal the issue to those who can." 
+
+GANDALF_ID='57a33f9b40f3a6eec05deab8'
+
+function getLastMessageUserIdForRoom() {
+    ROOM_ID=$1
+    curl -i \
+        -H "Accept: application/json" \
+        -H "Authorization: Bearer $GANDALF_GITTER_TOKEN" \
+        "https://api.gitter.im/v1/rooms/$ROOM_ID/chatMessages?limit=1" \
+        | grep -Po '"fromUser"((?!"id").)*"id"((?!").)*"[^"]+' \
+        | grep -Po '[^"]+$'
+}
+
+function postMessage() {
+    ROOM_ID=$1
+    curl -X POST -i \
+      -H "Content-Type: application/json" \
+      -H "Accept: application/json" \
+      -H "Authorization: Bearer $GANDALF_GITTER_TOKEN" \
+      "https://api.gitter.im/v1/rooms/$ROOM_ID/chatMessages" \
+      -d "{\"text\":\"$MESSAGE\"}"
+}
+
+function postIfNeeded () {
+    ROOM_ID=$1
+    ROOM_NAME=$2
+    ID=$(getLastMessageUserIdForRoom $ROOM_ID)
+
+    if [[ "$GANDALF_ID" = "$ID" ]]; then
+        echo "Nothing to do in $ROOM_NAME"
+    else
+        echo "Repling to message in $ROOM_NAME"
+        # postMessage $ROOM_ID
+    fi
+}
+
+echo $(postIfNeeded 5501985215522ed4b3dd2ac3 MithrilJS/mithril.js)
+echo $(postIfNeeded 5441311bdb8155e6700cc6f7 MithrilJS/mithril-node-render)
